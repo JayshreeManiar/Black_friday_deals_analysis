ba36b9c619ba8bf98789a45b282f134bacb54b80
Neeta90 <Neetashrivastava90.gmail,com>
2019-09-18 21:07:59 -0500

    updated data engineering file

--- templates/app.py
+++ templates/app.py
@@ -11,13 +11,13 @@ from sqlalchemy import create_engine
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
 
+app = Flask(__name__)
 ################################################
 # Database Setup
 #################################################
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///output.sqlite"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///blackfriday.sqlite"
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
 db = SQLAlchemy(app)
 
@@ -29,17 +29,32 @@ Base.prepare(db.engine, reflect=True)
 # Save references to each table
 BlackFriday = Base.classes.blackfriday
 #BlackFriday_new_data = Base.classes.BlackFriday_new_data
+session = Session(db.engine)
+
 
 
 @app.route("/")
-def index():
-    """Return the homepage."""
-    return render_template("index.html")
-
-
-@app.route("/about")
-def index():
-    """Return the aboutpage."""
-    return render_template("about.html")
+def Home():
+    """ Welcome"""
+    return (
+        f"Welcome<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/product_id<br/>"
+        
+    )
+@app.route("/api/v1.0/product_id")
+def product_id():
+    """Return a JSON list of stations from the dataset."""
   
+    # Query to list station name with ID
+    product = session.query(BlackFriday.age, BlackFriday.occupation).all()
+    # Convert list of tuples into normal list
+    all_product = list(np.ravel(product))
+    return jsonify(all_product)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port = 9999)
     
