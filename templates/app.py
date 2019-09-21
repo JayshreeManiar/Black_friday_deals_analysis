0f21710b0a001912ff2e8b3bc9534e3ab0e6e8ae
Neeta90 <Neetashrivastava90.gmail,com>
2019-09-21 10:21:10 -0500

    added new rout in app.py

--- templates/app.py
+++ templates/app.py
@@ -40,6 +40,7 @@ def Home():
         f"Welcome<br/>"
         f"Available Routes:<br/>"
         f"/api/v1.0/product_id<br/>"
+        f"/blackfridaydata/blackfridat"
         
     )
 @app.route("/api/v1.0/product_id")
@@ -51,6 +52,37 @@ def product_id():
     # Convert list of tuples into normal list
     all_product = list(np.ravel(product))
     return jsonify(all_product)
+
+ 
+@app.route("/blackfridaydata/blackfridat")
+#@app.route("/api/v1.0/blackfriday")
+def Filter_Search(blackfridat):
+    """Return the MetaData for a given sample."""
+    sel = [
+        BlackFriday.user_id,
+        BlackFriday.gender,
+        BlackFriday.age,
+        BlackFriday.occupation,
+        BlackFriday.product_category1,
+        BlackFriday.product_category2,
+        BlackFriday.product_category3,
+    ]
+
+    results = db.session.query(*sel).filter(BlackFriday.sample == sample).all()
+
+    # Create a dictionary entry for each row of metadata information
+    Filter_Search = {}
+    for result in results:
+        Filter_Search["user_id"] = result[0]
+        Filter_Search["gender"] = result[1]
+        Filter_Search["age"] = result[2]
+        Filter_Search["occupation"] = result[3]
+        Filter_Search["product_category1"] = result[4]
+        Filter_Search["product_category2"] = result[5]
+        Filter_Search["product_category3"] = result[6]
+
+    print(Filter_Search)
+    return jsonify(Filter_Search)
 
 
 if __name__ == '__main__':
